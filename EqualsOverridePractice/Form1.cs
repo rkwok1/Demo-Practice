4761193a23d60b4f280a4f020df488b5f35b4798
Ryan Kwok <rkwok2@studentmail.nait.ca>
2017-09-30 22:40:09 -0600

    finished demo

---
+++ EqualsOverridePractice/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
+
+
+namespace EqualsOverridePractice
+{
+    
+    public partial class Form1 : Form
+    {
+        //Global Members
+        private Random rnd = new Random();
+        private CDrawer drawSpace = new CDrawer(600, 900, false);
+        private List<ball> ballList = new List<ball>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            drawSpace.MouseLeftClick += DrawSpace_MouseLeftClick;
+        }
+
+        private void DrawSpace_MouseLeftClick(Point pos, CDrawer dr)
+        {
+            ball temp = new ball((float)(rnd.NextDouble() * 100 + 10), pos);
+
+            //Access the list to see if it contains a ball
+            lock (ballList)
+            {
+                if (ballList.Contains(temp))
+                    return; //return nothing!
+            }
+
+            //Add ball to the list
+            lock (ballList)
+            {
+                ballList.Add(temp);
+            }
+            Render();
+        }
+        private void Render()
+        {
+            drawSpace.Clear();
+
+            lock (ballList)
+            {
+                foreach  (ball b in ballList)
+                {
+                    b.Render(drawSpace);
+                }
+            }
+            drawSpace.Render();
+        }
+    }
+}
---
+++ EqualsOverridePractice/ball.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
+
+namespace EqualsOverridePractice
+{
+    class ball
+    {
+            private float size;
+            private PointF position;
+            private Color color;
+
+            public ball(float m_size, PointF m_position)
+            {
+                size = m_size;
+                position = m_position;
+                color = Color.ForestGreen;
+            }
+
+        
+        //Methods
+        private static float GetDistance(ball ballUno, ball ballDuo)
+        {
+            return (float)(Math.Sqrt(Math.Pow(ballDuo.position.X - ballUno.position.X, 2) + Math.Pow(ballDuo.position.Y - ballUno.position.Y, 2)));
+        }
+
+        public void Render(CDrawer canvas)
+        {
+            canvas.AddCenteredEllipse((int)position.X, (int)position.Y,
+                (int)size,
+                (int)size,
+                color,
+                1,
+                Color.Orchid);
+        }
+        public override bool Equals(object obj)
+        {
+            if(!(obj is ball))
+            {
+                return false;
+            }
+
+            ball temporaryBall = (ball)obj;
+
+            return (GetDistance(this, temporaryBall) < ((this.size / 2) + (temporaryBall.size / 2)));
+        }
+        public override int GetHashCode()
+        {
+            return 1;
+        }
+    }
+}
